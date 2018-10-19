10abd942ad9a5c09c354eebf07a3474975a31022
Boris Voskresenskiy <boris.voskresenskiy@endurance.com>
2018-10-18 19:42:14 -0500

    First set of changes fixing design bugs

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1IjoiYmFyYmVyNzYiLCJhIjoiY2puYzZzb2tqMWs0NzN2cGVlMDJxdjZ6bCJ9.a2KrDZnOqRISWbgtK0L0PQ',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
@@ -156,7 +156,7 @@ fillRestaurantsHTML = (restaurants = self.restaurants) => {
  * Create restaurant HTML.
  */
 createRestaurantHTML = (restaurant) => {
-  const li = document.createElement('li');
+  const li = document.createElement('div');
 
   const image = document.createElement('img');
   image.className = 'restaurant-img';
