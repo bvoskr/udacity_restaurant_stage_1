d2d540aea2606c279f564699a47cd3ca73f4e30c
Boris Voskresenskiy <boris.voskresenskiy@endurance.com>
2018-10-20 15:40:12 -0500

    Added activate handler to Service Worker

--- js/main.js
+++ js/main.js
@@ -222,10 +222,10 @@ addMarkersToMap = (restaurants = self.restaurants) => {
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('sw.js')
   .then(function(registration) {
-    console.log('Registration successful, scope is:', registration.scope);
+    console.log('[ServiceWorker] Registration successful, scope is:', registration.scope);
   })
   .catch(function(error) {
-    console.log('Service worker registration failed, error:', error);
+    console.log('[ServiceWorker] Registration failed, error:', error);
   });
 }
 
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const myCacheName = 'restaurantsSite';
+const currCacheName = 'restaurantsSite01';
 const filesToCache = ['index.html', 'restaurant.html', '/', '/js/main.js', '/js/restaurant_info.js', '/js/dbhelper.js', '/css/styles.css', '/data/restaurants.json'];
 for (let i = 1; i < 11; i++) {
 	filesToCache.push(`/img/${i}.jpg`);
@@ -6,7 +6,7 @@ for (let i = 1; i < 11; i++) {
 
 self.addEventListener('install', function(event) {
 	event.waitUntil(
-		caches.open(myCacheName).then(function(cache) {
+		caches.open(currCacheName).then(function(cache) {
 			return cache.addAll(filesToCache);
 		})
 	);
@@ -17,6 +17,22 @@ self.addEventListener('fetch', function(event) {
 		caches.match(event.request).then(function(response) {
 			if (response) return response;
 			return fetch(event.request);
+		})
+	);
+});
+
+self.addEventListener('activate', function(event) {
+	event.waitUntil(
+		caches.keys().then(function(cacheNames) {
+			return Promise.all(
+				cacheNames.filter(function(cacheName) {
+					return cacheName.startsWith('restaurantsSite') &&
+					       cacheName != currCacheName;
+				}).map(function(cacheName) {
+					console.log('[ServiceWorker] Removing old cache ', cacheName);
+					return caches.delete(cacheName);
+				})
+			);
 		})
 	);
 });
