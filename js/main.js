8ab202093091640eef664b782539fdaf4aea2a20
Boris Voskresenskiy <boris.voskresenskiy@endurance.com>
2018-10-21 09:16:13 -0500

    Accessibility: fixed headers level and breadcrumb ARIA attributes

--- js/main.js
+++ js/main.js
@@ -165,7 +165,7 @@ createRestaurantHTML = (restaurant) => {
   li.append(image);
   image.setAttribute('tabindex', '0');
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
   li.append(name);
   name.setAttribute('tabindex', '0');
